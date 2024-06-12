31462fd62d9bd81f9afdc18a5078caca3548fbab
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-06-12 16:52:25 -0600

    feat: grid ready

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Leslie Cardona</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript>
+      <link rel="stylesheet" href="assets/css/noscript.css" />
+    </noscript>
+  </head>
+  <body class="is-preload">
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+      <!-- Wrapper -->
+      <div id="wrapper">
+        <!-- Panel (Banner) -->
+        <section class="panel banner right">
+          <div class="content color0 span-3-75">
+            <h1 class="typewrite major" data-period="2000" data-type='[ "Hi, Im Leslie", "I am Creative", "I Love to manage", "I Love leading to success" ]'>
+              <span class="wrap"></span>
+            </h1>
+            <p>
+              "Innovation distinguishes between a leader and a follower."
+              -Steve Jobs, Apple co-founder
+            </p>
+            <ul class="actions">
+              <li>
+                <a href="#first" class="button primary color1 circle icon solid fa-angle-right">Next</a>
+              </li>
+            </ul>
+          </div>
+          <div class="image filtered span-1-75" data-position="25% 25%">
+            <img src="images/pic01.jpg" alt="" />
+          </div>
+        </section>
+
+        <!-- Panel -->
+
+        <!-- Panel (Spotlight) -->
+        <section class="panel spotlight large left" id="first">
+          <div class="content span-5">
+            <h2 class="major">I am a PM UX/UI Designer</h2>
+            <p>
+              "When people ask what I do, I explain that I provide organization and leadership for teams through project lifecycle phases to deliver a client's vision. My role involves working with diverse clients and projects, ensuring each day is unique and dynamic, which I thoroughly enjoy. I also bridge the gap between UX and UI design, ensuring seamless integration of user experience principles with user interface design. This involves collaborating closely with designers and developers to create cohesive, user-friendly products that meet client expectations and deliver superior user experiences. This blend of creativity and strategic planning makes my job both challenging and rewarding."
+            </p>
+          </div>
+          <div class="image filtered tinted" data-position="top right">
+            <img src="images/pic03.jpg" alt="" />
+          </div>
+        </section>
+
+        <section class="panel">
+          <div class="intro color2">
+            <h2 class="major">In a few words, I'm feeling myself like a:</h2>
+            <p>I am creative. I turn visions into reality through strategic design and management. I love to manage. Bringing order to chaos is my superpower. I thrive on leading to success. Every project is a journey from concept to completion.</p>
+          </div>
+          <div class="gallery">
+            <div class="group span-3">
+              <a href="images/gallery/fulls/01.jpg" class="image filtered span-3" data-position="bottom"><img src="images/gallery/thumbs/01.jpg" alt="" /></a>
+              <a href="images/gallery/fulls/02.jpg" class="image filtered span-1-5" data-position="center"><img src="images/gallery/thumbs/02.jpg" alt="" /></a>
+              <a href="images/gallery/fulls/03.jpg" class="image filtered span-1-5" data-position="bottom"><img src="images/gallery/thumbs/03.jpg" alt="" /></a>
+            </div>
+          </div>
+        </section>
+
+        <!-- Panel -->
+        <section class="panel color4-alt">
+          <div class="inner columns divided">
+            <div class="span-1-5">
+              <ul class="contact-icons color1">
+                <a href="https://www.facebook.com/leslie.cardona3" target="_blank">
+                  <li class="icon brands fa-facebook-f "></li>
+                </a>
+                <a href="https://www.instagram.com/lesliegcardona/" target="_blank">
+                  <li class="icon brands fa-instagram"></li>
+                </a>
+                <a href="https://www.linkedin.com/in/leslie-cardona-9863361a0/" target="_blank">
+                  <li class="icon brands fa-linkedin"></li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <!-- Panel -->
+
+        <!-- Copyright -->
+        <div class="copyright">
+          Made with <i class="icon fa-solid fa-heart"></i> by
+          <a href="https://tonatiuhgomez.com/">tonatiuhgomez.com</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/type.js"></script>
+  </body>
+</html>
